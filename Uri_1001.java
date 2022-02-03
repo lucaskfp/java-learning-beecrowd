dc0e13b852545306f175ebe010e248ac05fcbd91
Lucas Kennedy <lukakfp@gmail.com>
2022-02-03 10:54:53 -0300

    solution 1001

---
+++ Uri_1001.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Uri_1001 {
+    public static void main(String args[]) {
+        int a, b, x;
+
+        Scanner teclado = new Scanner(System.in);
+
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+
+        x = a + b;
+
+        System.out.println("X = " + x);
+
+        teclado.close();
+    }
+}
